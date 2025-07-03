2e6a956ee69b396584de35d8379fc45a29cb13f9
Trasis33 <fredrik.langa@gmail.com>
2025-07-03 22:41:29 +0200

    Remove unused Link import from Recurring.js

--- client/src/pages/Recurring.js
+++ client/src/pages/Recurring.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
 import apiClient from '../api/axios';
 import formatCurrency from '../utils/formatCurrency';
 
